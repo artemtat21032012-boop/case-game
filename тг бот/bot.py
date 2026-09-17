b056bfa069c4643aea993099e2cdd15eec50f209
artemtat21032012-boop <artemtat21032012@gmail.com>
2026-09-17 08:49:08 +0000

    Update bot.py

--- тг бот/bot.py
+++ тг бот/bot.py
@@ -2,7 +2,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppI
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 
-TOKEN = "8610528069:AAH3H-ryTzTfRkx880lDTSt6sSFk5gz_Fg4"
+TOKEN = "твой токен"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
